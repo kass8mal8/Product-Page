13b9b6e6037cbe26d7f55e619c884eff22a2969b
kass8mal8 <kassimaly21@gmail.com>
2024-06-06 18:42:50 +0300

    fade animation and crat tweaks

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,46 +5,53 @@ import Btn from "./Btn";
 import delIcon from "../images/icon-delete.svg";
 
 const Cart = () => {
-    const { count, image, setCount, setIsCart } = useContext(ProductContext);
+	const { count, image, setCount, setIsCart, isClicked } =
+		useContext(ProductContext);
 
-    const handleClear = () => {
-        setCount(0)
-        setIsCart(false);
-    }
+	const handleClear = () => {
+		setCount(0);
+		setIsCart(false);
+	};
 
-    return (  
-        <Box className="product-cart">
-            <Box className='cart-head'> 
-                <Typography variant='h6'>Cart</Typography> 
-            </Box>
-            <Divider />
-            <Box className="cart-content">
-                { count === 0 
-                    ? <Typography variant='body1' color='text.secondary'>
-                        Your Cart is Empty
-                    </Typography>
-                    : <Box>
-                        <Box className='row'>
-                            <img src={image} alt={"product"} className="image" />
-                            <Box>
-                                <Typography variant='body1' color='text.secondary'>
-                                    Fall Limited Edition Sneakers
-                                </Typography>
-                                <Stack direction={'row'} spacing={3} alignItems={'center'}>
-                                    <Typography variant='body1' color='text.secondary'>$125.00 x {count}</Typography>
-                                    <Typography variant="h6">${125*count}</Typography>
-                                </Stack>
-                            </Box>
-                            <IconButton sx={{ width: '5%', height: '5%' }} onClick={handleClear}>
-                                <img src={delIcon} alt="delete" height={20} />
-                            </IconButton>
-                        </Box>
-                        <Btn text={"checkout"} icon={null} />
-                      </Box>
-                }
-            </Box>
-        </Box>
-    );
-}
- 
+	return (
+		<Box className="product-cart">
+			<Box className="cart-head">
+				<Typography variant="h6">Cart</Typography>
+			</Box>
+			<Divider />
+			<Box className="cart-content">
+				{isClicked && count > 0 ? (
+					<Box>
+						<Box className="row">
+							<img src={image} alt={"product"} className="image" />
+							<Box>
+								<Typography variant="body1" color="text.secondary">
+									Fall Limited Edition Sneakers
+								</Typography>
+								<Stack direction={"row"} spacing={3} alignItems={"center"}>
+									<Typography variant="body1" color="text.secondary">
+										$125.00 x {count}
+									</Typography>
+									<Typography variant="h6">${125 * count}</Typography>
+								</Stack>
+							</Box>
+							<IconButton
+								sx={{ width: "5%", height: "5%" }}
+								onClick={handleClear}
+							>
+								<img src={delIcon} alt="delete" height={20} />
+							</IconButton>
+						</Box>
+						<Btn text={"checkout"} icon={null} />
+					</Box>
+				) : (
+					<Typography variant="body1" color="text.secondary">
+						Your Cart is Empty
+					</Typography>
+				)}
+			</Box>
+		</Box>
+	);
+};
+
 export default Cart;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,45 @@
 import { IconButton, Stack, Typography } from "@mui/material";
 import { useContext, useEffect } from "react";
 import { ProductContext } from "../App";
-import prodImage from "../images/image-product-1.jpg"
+import prodImage from "../images/image-product-1.jpg";
 
-import plus from "../images/icon-plus.svg"
-import minus from "../images/icon-minus.svg"
+import plus from "../images/icon-plus.svg";
+import minus from "../images/icon-minus.svg";
 
 const Counter = () => {
-    const {count, setCount, setIsClicked, setImage} = useContext(ProductContext)
+	const { count, setCount, setIsClicked, setImage } =
+		useContext(ProductContext);
 
-    const handleSubtraction = () => {
-        if(count < 1) return
-        else {
-            setCount(count - 1)
-        }
-        if(count === 1) setIsClicked(false)
-    }
+	const handleSubtraction = () => {
+		if (count < 1) return;
+		else {
+			setCount(count - 1);
+		}
+		if (count === 1) setIsClicked(false);
+	};
 
-    useEffect(() => {
-        count >= 1 && setImage(prodImage)
-    }, [count, setImage]);
+	useEffect(() => {
+		count >= 1 && setImage(prodImage);
+	}, [count, setImage]);
 
-    return (  
-        <Stack direction='row' spacing={2} alignItems={'center'} className="counter">
-            <IconButton onClick={handleSubtraction}> <img src={minus} alt={'minus'} />  </IconButton>
-            <Typography sx={{ fontWeight: 'bold' }}>{count}</Typography>
-            <IconButton onClick={() => setCount(count + 1)}> <img src={plus} alt="plus" /> </IconButton>
-        </Stack>
-    );
-}
- 
+	return (
+		<Stack
+			direction="row"
+			spacing={2}
+			alignItems={"center"}
+			className="counter"
+		>
+			<IconButton onClick={handleSubtraction}>
+				{" "}
+				<img src={minus} alt={"minus"} />{" "}
+			</IconButton>
+			<Typography sx={{ fontWeight: "bold" }}>{count}</Typography>
+			<IconButton onClick={() => setCount(count + 1)}>
+				{" "}
+				<img src={plus} alt="plus" />{" "}
+			</IconButton>
+		</Stack>
+	);
+};
+
 export default Counter;
